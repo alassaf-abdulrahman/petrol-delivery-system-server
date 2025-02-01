42fa593ae0cd5e22e53516b2698fd9bc5d82b726
ABDULRAHMAN ALASSAF <abodoassaf112@gmail.com>
2025-02-01 18:32:04 +0800

    adding the api file

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', UserController::class . '@login');
+Route::post('/register', UserController::class . '@createUserAccount');
+Route::put('/update-profile', UserController::class . '@updateUserAccount');
+
+Route::prefix('customer')->group(function () {
+    Route::post('/place-fuel-order', CustomerController::class . '@placeFuelOrder');
+    Route::get('/tarck-fuel-order', CustomerController::class . '@trackFuelOrder');
+    Route::post('/make-payment', CustomerController::class . '@makePayment');
+});
+
+Route::prefix('driver')->group(function () {
+    Route::get('/fuel-orders', DriverController::class . '@getFuelOrders');
+    Route::put('/accept-fuel-order', DriverController::class . '@acceptFuelOrder');
+    Route::put('/confirm-delivery', DriverController::class . '@confirmFuelDelivery');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/fuel-orders', AdminController::class . '@getFuelsOrdersList');
+    Route::put('/assign-driver', AdminController::class . '@assignDriverToFuelOrder');
+    Route::put('/reject-order', AdminController::class . '@rejectFuelOrder');
+    Route::post('/generate-report', AdminController::class . '@generateMonthlyReport');
+});
